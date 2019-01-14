c539c80a17cfc4ab3b26c607e108632a87a63621
GregoireBesson <gregoire.besson@epfl.ch>
2019-01-14 15:03:17 -0800

    can now run several generations, replace the k worst individual with k new random individuals for the next gen

--- superBallJumpingV2/runMe.m
+++ superBallJumpingV2/runMe.m
@@ -47,10 +47,6 @@ strings = [1  1   1  1  2  2  2  2  3  3  3  3  4  4  4  4  5  5  6  6  7  7  8 
 % each column contain the pair of colum numbers in strings to actuate
 actuators = [1  5  9 13 17 19 21 23 11  3 12  4;
              2  6 10 14 18 20 22 24 15  7 16  8];
-       
-% Evolution parameteres
-nbActuators = 6;                            % should be in [1;12]
-delayAct = 0;                               % in ms
 
 % Compute rest lengths from a certain pretension
 l0 = norm(nodes(1,:)-nodes(7,:));       % initial string length
@@ -66,107 +62,151 @@ nodes = (My(1:3,1:3)*nodes')';
 CoMz = barLength/2;                         % (m)
 nodes(:,3) = nodes(:,3) + CoMz;             % shift all the nodes in z
 
-%% Start a simulation for each individual
-nbIndividuals = 1;
-nbActuations = 2;
+%% Evolution parameters
+
+selectionMode = 'random';       % selectionMode can be 'manual' or 'random'
+nbActuators = 6;                % should be in [1;12]
+delayAct = 0;                   % in ms
+nbIndividuals = 3;              % size of population
+nbGeneration = 2;               % number of generation
+nbActuations = 1;               % size of actuation sequence
+k = 2;                          % selection parameter (remove k worst ind.)
+Fitness = 'dist';               % perf to be evaluated (jump, dist or both)              
+
 TraveledDist = zeros(nbIndividuals,1);
 Zmax = zeros(nbIndividuals,1);
 actuatedStrings = zeros(nbIndividuals, nbActuations, 2*nbActuators);
 
-for i = 1:nbIndividuals
-ActuationCounter = 0;
-% selectionMode can be 'manual' or 'random'
-selectionMode = 'manual';
-
 %Create the random number stream for reproducibility:
-rngParameters = RandStream('mlfg6331_64','Seed','Shuffle'); 
+rngParameters = RandStream('mlfg6331_64','Seed','Shuffle');
 
-% randomm actuators for evolution
-[actuatedStrings,ActuationCounter] = randomStrings(actuators,nbActuators,rngParameters,actuatedStrings,i,ActuationCounter);
-
-% manual actuators for testing
-if (strcmpi(selectionMode,'manual'))
-    actuatedStrings(i,1,:) = [13 14 9 10 3 7 19 20 23 24 1 2]; 
-    actuatedStrings(i,2,:) = [3 7 5 6 19 20 9 10 21 22 12 16];
+for g = 1:nbGeneration
+    for i = 1:nbIndividuals
+        ActuationCounter = 0;
+        
+        % randomm actuators for evolution
+        if ( g==1 || strcmpi(selectionMode,'random'))
+            [actuatedStrings,ActuationCounter] = randomStrings(actuators,nbActuators,rngParameters,actuatedStrings,i,ActuationCounter);
+        end
+        
+        % manual actuators for testing
+        if (strcmpi(selectionMode,'manual'))
+            %actuatedStrings(i,1,:) = [13 14 9 10 3 7 19 20 23 24 1 2];
+            %actuatedStrings(i,2,:) = [3 7 5 6 19 20 9 10 21 22 12 16];
+            
+            %actuatedStrings = [3 7 5 6 19 20 9 10 21 22 12 16]; %to show an upright mvt
+            %actuatedStrings = [9 10 11 15 23 24 17 18 4 8 5 6]; %to start in good pos
+            %actuatedStrings = [9 10];                  %1 lower actuation
+            %actuatedStrings = [9 10 11 15];            %2 lower actuations
+            %actuatedStrings = [9 10 11 15 23 24];      %3 lower actuations
+            %actuatedStrings = [9 10 19 20];            %1 lower and 1 upper actuation
+            %actuatedStrings = [17 18];                 %1 actuation to test stiffness
+            %actuatedStrings = [ 9 10;
+            %                   23 24;
+            %                    3  7];                 % serial actuation (row by row)
+        end
+        
+        %% Creation of the structure
+        
+        % Simulation and plot timesteps
+        delT = 0.001;                   % timestep for dynamic sim in seconds
+        delTUKF  = 0.001;               % timestep for the U Kalman Filter
+        
+        % creation of the object superBall
+        superBall = TensegrityStructure(nodes, strings, bars, F, stringStiffness,...
+            barStiffness, stringDamping, nodalMass, delT, delTUKF, ...
+            stringRestLength, selectionMode);
+        
+        %% Create dynamics display
+        
+        superBallDynamicsPlot = TensegrityPlot(nodes, strings, bars, ...
+            bar_radius, string_radius);
+        f = figure('units','normalized','outerposition',[0 0 1 1]);
+        % use a method within TensegrityPlot class to generate a plot of the
+        % structure
+        generatePlot(superBallDynamicsPlot,gca);
+        updatePlot(superBallDynamicsPlot);
+        
+        %settings to make it pretty
+        axis equal
+        view(90, 0); % X-Z view
+        view(3)
+        grid on
+        light('Position',[0 0 10],'Style','local')
+        lighting flat
+        colormap([0.8 0.8 1; 0 1 1]);
+        lims = 2*barLength;
+        xlim([-1.2*lims 1.2*lims])
+        ylim([-1.2*lims 1.2*lims])
+        zlim(1*[-0.01 lims])
+        % plot the ground
+        hold on
+        [x, y] = meshgrid(-3*barLength:0.1:3*barLength); % Generate x and y data
+        z = -bar_radius*ones(size(x, 1)); % Generate z data
+        C = 2*x.*y;
+        ground = surf(x, y, z); % Plot the surface
+        ground.EdgeColor = 'none';
+        
+        drawnow; % Draw and hold initial conditions
+        pause(0);
+        
+        %% Run dynamics
+        
+        displayTimespan = 1/20;     % 20fps
+        % set the dynamics parameters
+        [~,~] = myDynamicsUpdate(superBall, superBallDynamicsPlot,...
+            displayTimespan, actuatedStrings, pretension, maxTension, l0,...
+            actuators, nbActuators,rngParameters,i,ActuationCounter,nbActuations);
+        
+        nbLoop = round((100/100)*180*nbActuations); %2000 -> 100sec de simulation
+        
+        % Simulation loop
+        for l = 1:nbLoop
+            [actuatedStrings,ActuationCounter] = myDynamicsUpdate();
+            
+            %display Data 'PostSim' or 'RealTime' (make simulation much slower!)
+            plotData(superBall,superBallDynamicsPlot,displayTimespan,...
+                l,nbLoop,'PostSim');
+        end
+        
+        %% Simulation results
+        
+        TraveledDist(i) = superBall.TraveledDist;
+        Zmax(i) = superBall.Zmax;
+        
+    end
     
-    %actuatedStrings = [3 7 5 6 19 20 9 10 21 22 12 16]; %to show an upright mvt
-    %actuatedStrings = [9 10 11 15 23 24 17 18 4 8 5 6]; %to start in good pos
-    %actuatedStrings = [9 10];                  %1 lower actuation
-    %actuatedStrings = [9 10 11 15];            %2 lower actuations
-    %actuatedStrings = [9 10 11 15 23 24];      %3 lower actuations
-    %actuatedStrings = [9 10 19 20];            %1 lower and 1 upper actuation
-    %actuatedStrings = [17 18];                 %1 actuation to test stiffness
-    %actuatedStrings = [ 9 10;
-    %                   23 24;
-    %                    3  7];                 % serial actuation (row by row)
-end
-
-%% Creation of the structure
-
-% Simulation and plot timesteps
-delT = 0.001;                   % timestep for dynamic sim in seconds
-delTUKF  = 0.001;               % timestep for the U Kalman Filter
-
-% creation of the object superBall
-superBall = TensegrityStructure(nodes, strings, bars, F, stringStiffness,...
-    barStiffness, stringDamping, nodalMass, delT, delTUKF, ...
-    stringRestLength, selectionMode);
-
-%% Create dynamics display
-
-superBallDynamicsPlot = TensegrityPlot(nodes, strings, bars, ...
-    bar_radius, string_radius);
-f = figure('units','normalized','outerposition',[0 0 1 1]);
-% use a method within TensegrityPlot class to generate a plot of the
-% structure
-generatePlot(superBallDynamicsPlot,gca);
-updatePlot(superBallDynamicsPlot);
-
-%settings to make it pretty
-axis equal
-view(90, 0); % X-Z view
-view(3)
-grid on
-light('Position',[0 0 10],'Style','local')
-lighting flat
-colormap([0.8 0.8 1; 0 1 1]);
-lims = 2*barLength;
-xlim([-1.2*lims 1.2*lims])
-ylim([-1.2*lims 1.2*lims])
-zlim(1*[-0.01 lims])
-% plot the ground
-hold on
-[x, y] = meshgrid(-3*barLength:0.1:3*barLength); % Generate x and y data
-z = -bar_radius*ones(size(x, 1)); % Generate z data
-C = 2*x.*y;
-ground = surf(x, y, z); % Plot the surface
-ground.EdgeColor = 'none';
-
-drawnow; % Draw and hold initial conditions
-pause(0);
-
-%% Run dynamics
-
-displayTimespan = 1/20;     % 20fps
-% set the dynamics parameters
-[~,~] = myDynamicsUpdate(superBall, superBallDynamicsPlot,...
-    displayTimespan, actuatedStrings, pretension, maxTension, l0,...
-    actuators, nbActuators,rngParameters,i,ActuationCounter,nbActuations);
-
-nbLoop = round((100/100)*180*nbActuations); %2000 -> 100sec de simulation
-
-% Simulation loop
-for l = 1:nbLoop
-    [actuatedStrings,ActuationCounter] = myDynamicsUpdate();
+    %% Select which performance to be evaluated
     
-    %display Data 'PostSim' or 'RealTime' (make simulation much slower!)
-    plotData(superBall,superBallDynamicsPlot,displayTimespan,...
-        l,nbLoop,'PostSim');
-end
-
-%% Simulation results
-
-TraveledDist(i) = superBall.TraveledDist;
-Zmax(i) = superBall.Zmax;
+    if (strcmpi(Fitness,'jump'))
+        Performance = Zmax;
+    elseif (strcmpi(Fitness,'dist'))
+        Performance = TraveledDist;
+    else
+        Performance = TraveledDist.*Zmax;
+    end
+    
+    % sort the perfomances
+    [sortedPerformance, Indexes] = sort(Performance,'ascend');
+    
+    %% Mutation
+    % send the previous sequences to the next
+    pastActuatedStrings = actuatedStrings(:,nbActuations,:);
+    nextActuatedStrings = actuatedStrings(:,nbActuations,:);
+    % replace the k worst indiv with mutated version of the k best
+    % OR with random strings
+    for i = 1:k
+        for a = 0:(nbActuations-1)
+            [nextActuatedStrings,~] = randomStrings(actuators,nbActuators,...
+                rngParameters,nextActuatedStrings,Indexes(i),a);
+        end
+    end
+    
+    %update the strings to actuate for next generation
+    actuatedStrings = nextActuatedStrings;
+    
+    % force the next selection mode to manual cause we have now defined
+    % sequence
+    selectionMode = 'manual';
 
 end
