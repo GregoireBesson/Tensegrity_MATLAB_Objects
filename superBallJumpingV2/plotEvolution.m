235e6e54434e0ceb5d1cb08f97808750d0c74224
GregoireBesson <gregoire.besson@epfl.ch>
2019-01-23 17:02:04 -0800

    Plot now the average number of motors used by the best indiv of each generation on final evolution plot, git ignores jpg

--- superBallJumpingV2/plotEvolution.m
+++ superBallJumpingV2/plotEvolution.m
@@ -12,20 +12,24 @@
 % Output: - plot of avg perf over the generations
 %         - box plot of perf over the generations
 
-function plotEvolution(performance,Fitness,nbActuators,nbGeneration,nbIndividuals,nbActuations,k,p)
+function plotEvolution(performance,Fitness,nbActuators,nbGeneration,nbIndividuals,nbActuations,k,p,nbAvgActuatorsBestIndiv)
 
     generationsVector = 1:nbGeneration;
     avgPerformance = mean(performance);
     
     figure();
+    yyaxis left
     plot(generationsVector, avgPerformance,'r','LineWidth',1.5)
     hold on
     boxplot(performance);
-    title([num2str(nbActuations), ' actuation cycles, ', num2str(nbIndividuals), ' indiv, k = ',num2str(k),', p = ',num2str(p)]);
+    title([num2str(nbActuations), ' actuation cycles, ', num2str(nbIndividuals), ' indiv, k = ',num2str(k),', p = ',num2str(p),', ',num2str(nbActuators), ' init actuators']);
     xlabel('Generation');
     ylabel(['Performance: ', Fitness, ' (m)']);
+    yyaxis right
+    plot(generationsVector,nbAvgActuatorsBestIndiv,'--','LineWidth',1)
+    ylabel('Average number of motors per cycle (Best Indiv.)')
     grid on
-    legend('Average performance')
+    legend('Avg performance','Avg nb of motors')
     set(gca,'fontsize', 14);
     
 end
