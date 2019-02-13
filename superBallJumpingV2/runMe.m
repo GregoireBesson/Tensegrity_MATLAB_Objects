d657d12c7e850a7e0a646bae9dc358d2dfbb0f46
GregoireBesson <gregoire.besson@epfl.ch>
2019-02-13 13:46:33 -0800

    clean code

--- superBallJumpingV2/runMe.m
+++ superBallJumpingV2/runMe.m
@@ -49,8 +49,8 @@ bars = [1:2:11;
         2:2:12];
     
 % string connectivity
-strings = [1  1   1  1  2  2  2  2  3  3  3  3  4  4  4  4  5  5  6  6  7  7  8  8;
-           7  8  10 12  5  6 10 12  7  8  9 11  5  6  9 11 11 12  9 10 11 12  9 10];
+strings = [1 1  1  1 2 2  2  2 3 3 3  3 4 4  4  4  5  5  6  6  7  7  8  8;
+           7 8 10 12 5 6 10 12 7 8 9 11 5 6  9 11 11 12  9 10 11 12  9 10];
 
 % each column contain the pair of colum numbers in strings to actuate
 % the column number correspond to the motor number in the NetworkMap
@@ -81,15 +81,18 @@ displayData = 'NoPlot';
 displaySimulation = false;      % boolean to display every simulation
 saveResults = true;             % boolean to save results in a mat file
 initMode = 'random';            % actuators selection, 'manual' or 'random'
-filename = 'output/distToGoal4over6MotI15G30c3t2.mat';
+filename = 'output/distToGoal4over6MotI1000g1c3t2.mat';
+% draw rndm available actuators for every indiv if true
+changeAvlActuators = true;
 % number of end cap actuated on the robot 
-nbAvlbleActuators = 6; %1+round(rand*(nbMotorsMax-1));  % should be in [1;12] 
-% number of end cap actuated simultaneously should be in [1:nbAvlbleActuators]
+nbAvlbleActuators = 6; %1+round(rand*(nbMotorsMax-1));% should be in [1;12] 
+%number of endcap actuated simultaneously should be in[1:nbAvlbleActuators]
 nbUsedActuators = 4; %1+round(rand*(nbAvailableActuators-1)); 
 % update the weight of the robot considering the number of actuators
-nodalMass = (nbAvlbleActuators*weightActEndCap +  (12-nbAvlbleActuators)*weightEmptyEndCap)/12 *ones(12,1);
-nbIndividuals = 15;              % size of population
-nbGeneration = 30;               % number of generation
+nodalMass = (nbAvlbleActuators*weightActEndCap +  ...
+    (12-nbAvlbleActuators)*weightEmptyEndCap)/12 *ones(12,1);
+nbIndividuals = 1000;           % size of population
+nbGeneration = 1;               % number of generation
 nbActuationCycle = 3;           % size of actuation sequence
 delayAct = 0;                   % in ms
 
@@ -104,7 +107,6 @@ t = 2;                          % tournament size (tournament selection)
 elitism = true;                 % copy e elites without mutation if true
 e = 1;                          % number of elites to be copied 
 
-
 % Mutation parameters
 if (nbUsedActuators == nbAvlbleActuators)
     mutation = 'soft';
@@ -115,8 +117,10 @@ p = 0.2;                        % probability of mutation
 
 %Create the random number stream for reproducibility:
 rngParameters = RandStream('mlfg6331_64','Seed','Shuffle');
+
 %Draw a random set of location for the actuators, same for all the indiv
-AvlblActuators = datasample(rngParameters,1:12,nbAvlbleActuators,'Replace',false);
+AvlblActuators = datasample(rngParameters,1:12,nbAvlbleActuators,...
+    'Replace',false);
 % OR user choose the end cap that are actuated (comment if you want rndm)
 %AvlblActuators = [1 4 5 8 10 11]; % 1 motor on each rod
 
@@ -132,7 +136,8 @@ nbAvgActuatorsBestIndiv = zeros(1,nbGeneration);
 for g = 1:nbGeneration
     
      % Status Flag
-     fprintf('------------------- Generation    %d/%d -------------------\n',g,nbGeneration);
+     fprintf('------------------ Generation %d/%d ------------------\n',...
+         g,nbGeneration);
     
     for i = 1:nbIndividuals
         
@@ -144,15 +149,21 @@ for g = 1:nbGeneration
         
         % Init actuators at the first generation
         if ( g == 1 )
+            if (changeAvlActuators)
+                %Draw a random set of location for the actuators, 
+                %same for all the indiv
+                AvlblActuators = datasample(rngParameters,1:12,...
+                    nbAvlbleActuators,'Replace',false);
+            end
             % random initialization
             if (strcmpi(initMode,'random'))
-                %[actuatedStrings, genes, actuationCycleCounter] = randomStrings(actuators,nbActuators,rngParameters,actuatedStrings,genes,i,g,actuationCycleCounter);
                 genes(1,i,:,:) = initRandomGenome(nbActuationCycle,...
                              AvlblActuators,nbUsedActuators,rngParameters);
             % manual initialization for testing
             elseif (strcmpi(initMode,'manual'))
                 % load the best individual and the genome from previous run
-                load('output/jump6over12MotI100G1c1T2.mat','BestIndividual','savedGenes','fitness','oldPerf','oldPerfIndexes');
+                load(filename,'BestIndividual','savedGenes','fitness',...
+                    'oldPerf','oldPerfIndexes');
                 % to just show the BestIndividual from a specific run
                 %genes(1,1,:,:) = BestIndividual;
                 % to continue an evolution from the last run
@@ -168,19 +179,21 @@ for g = 1:nbGeneration
                 
                 %actuatedStrings(i,1,:) = [13 14 9 10 3 7 19 20 23 24 1 2];
                 %actuatedStrings(i,2,:) = [3 7 5 6 19 20 9 10 21 22 12 16];
-                %actuatedStrings = [3 7 5 6 19 20 9 10 21 22 12 16]; %to show an upright mvt
-                %actuatedStrings = [9 10 11 15 23 24 17 18 4 8 5 6]; %to start in good pos
-                %actuatedStrings = [9 10];                  %1 lower actuation
-                %actuatedStrings = [9 10 11 15];            %2 lower actuations
-                %actuatedStrings = [9 10 11 15 23 24];      %3 lower actuations
-                %actuatedStrings = [9 10 19 20];            %1 lower and 1 upper actuation
-                %actuatedStrings = [17 18];                 %1 actuation to test stiffness
+                %to show an upright mvt:
+                %actuatedStrings = [3 7 5 6 19 20 9 10 21 22 12 16]; 
+                %to start in good pos:
+                %actuatedStrings = [9 10 11 15 23 24 17 18 4 8 5 6]; 
+                %actuatedStrings = [9 10];              %1 lower actuation
+                %actuatedStrings = [9 10 11 15];        %2 lower actuations
+                %actuatedStrings = [9 10 11 15 23 24];  %3 lower actuations
+                %actuatedStrings = [9 10 19 20];  %1 lower and 1 upper act
+                %actuatedStrings = [17 18];       %1 actuation to test stif
                 %actuatedStrings = [ 9 10;
                 %                   23 24;
-                %                    3  7];                 % serial actuation (row by row)
+                %                    3  7]; % serial actuation (row by row)
                 
             else
-                error('Initialization mode unknown, enter random or manual');
+                error('Initialization mode unknown, random or manual');
             end
         end
         
@@ -191,18 +204,21 @@ for g = 1:nbGeneration
         
         % recreate structure and run simulation only on new individuals
         if ( g==1 || ...
-             g>1 && (strcmpi(selectionMode,'ranking')) && ismember(i,perfIndexes(1:end-k)) || ...
-             g>1 && (strcmpi(selectionMode,'tournament')) && elitism==false || ...
-             g>1 && (strcmpi(selectionMode,'tournament')) && elitism && i>e ) 
+             g>1 && (strcmpi(selectionMode,'ranking')) && ...
+             ismember(i,perfIndexes(1:end-k)) || ...
+             g>1 && (strcmpi(selectionMode,'tournament')) && ...
+             elitism==false || ...
+             g>1 && (strcmpi(selectionMode,'tournament')) && ...
+             elitism && i>e ) 
         
             % Simulation and plot timesteps
-            delT = 0.001;                   % timestep for dynamic sim in seconds
-            delTUKF  = 0.001;               % timestep for the U Kalman Filter
+            delT = 0.001;           % timestep for dynamic sim in seconds
+            delTUKF  = 0.001;       % timestep for the U Kalman Filter
             
             % creation of the object superBall
-            superBall = TensegrityStructure(nodes, strings, bars, F, stringStiffness,...
-                barStiffness, stringDamping, nodalMass, delT, delTUKF, ...
-                stringRestLength, initMode);
+            superBall = TensegrityStructure(nodes, strings, bars, F, ...
+                stringStiffness,barStiffness, stringDamping, nodalMass, ...
+                delT, delTUKF, stringRestLength, initMode);
             
             %% Create dynamics display
             
@@ -212,8 +228,8 @@ for g = 1:nbGeneration
             if (displaySimulation)
                 
                 f = figure('units','normalized','outerposition',[0 0 1 1]);
-                % use a method within TensegrityPlot class to generate a plot of the
-                % structure
+                % use a method within TensegrityPlot class to generate 
+                % a plot of the structure
                 generatePlot(superBallDynamicsPlot,gca);
                 updatePlot(superBallDynamicsPlot);
                 
@@ -231,7 +247,7 @@ for g = 1:nbGeneration
                 zlim(1*[-0.01 lims])
                 % plot the ground
                 hold on
-                [x, y] = meshgrid(-4*barLength:0.1:4*barLength); % Generate x and y data
+                [x, y] = meshgrid(-4*barLength:0.1:4*barLength); 
                 z = -bar_radius*ones(size(x, 1)); % Generate z data
                 C = 2*x.*y;
                 ground = surf(x, y, z); % Plot the surface
@@ -255,9 +271,10 @@ for g = 1:nbGeneration
             % set the dynamics parameters
             myDynamicsUpdate(superBall, superBallDynamicsPlot,...
                 displayTimespan, pretension, maxTension, l0,...
-                actuators,i, nbActuationCycle, displaySimulation,genes,g, firstStringsToActuate);
+                actuators,i, nbActuationCycle, displaySimulation,genes,...
+                g,firstStringsToActuate);
             
-            nbLoop = round(280*nbActuationCycle); %2000 -> 100sec de simulation
+            nbLoop = round(280*nbActuationCycle);
             
             % Simulation loop
             for l = 1:nbLoop
@@ -302,7 +319,8 @@ for g = 1:nbGeneration
     % copy the perf of the unchanged indiv from last generation
     if (g > 1)
         if (strcmpi(selectionMode,'ranking'))
-            performance(perfIndexes(k+1:end),g) = performance(perfIndexes(k+1:end),g-1);
+            performance(perfIndexes(k+1:end),g) = ...
+                performance(perfIndexes(k+1:end),g-1);
         elseif ((strcmpi(selectionMode,'tournament')) && elitism)           % TODO correct BUG if e>2
             performance(1:e,g) = sortedPerformance(end);
         end
@@ -315,12 +333,14 @@ for g = 1:nbGeneration
     
     % compute the average number of actuator per cycle used by BestInd
     if strcmpi(initMode,'random')
-        nbAvgActuatorsBestIndiv(g) = length( find(genes(g,perfIndexes(end),:,:)==1) )/nbActuationCycle;
+        nbAvgActuatorsBestIndiv(g) = ...
+          length(find(genes(g,perfIndexes(end),:,:)==1) )/nbActuationCycle;
     end
     
     if ( (g < nbGeneration) && (strcmpi(selectionMode,'tournament')) )
         % fill the next generation with tournament method
-        genes = tournamentSelection(genes,nbIndividuals,performance,g,t,elitism,e,sortingMode);
+        genes = tournamentSelection(genes,nbIndividuals,performance,g,t,...
+            elitism,e,sortingMode);
     end
 
     %% Mutation
@@ -328,7 +348,9 @@ for g = 1:nbGeneration
     % mutate the individuals that need to be mutated depending on the
     % selection method
     if (g < nbGeneration)
-        genes = genesMutation(mutation,genes,g,nbIndividuals,perfIndexes,AvlblActuators,nbActuationCycle,k,p,selectionMode,elitism,e);
+        genes = genesMutation(mutation,genes,g,nbIndividuals,...
+            perfIndexes,AvlblActuators,nbActuationCycle,k,p,...
+            selectionMode,elitism,e);
     end
     
     
@@ -343,7 +365,8 @@ for g = 1:nbGeneration
         savedGenes = genes;
         oldPerf = performance;
         oldPerfIndexes = perfIndexes;
-        save(filename,'BestIndividual','savedGenes','fitness','oldPerf','oldPerfIndexes')
+        save(filename,'BestIndividual','savedGenes','fitness',...
+            'oldPerf','oldPerfIndexes')
         fprintf('Results saved \n');
     end
 end
@@ -353,5 +376,7 @@ end
 fprintf('Evolution complete !\n');
 
 if (saveResults) 
-    plotEvolution(performance,sortedPerformance,fitness,nbAvlbleActuators,nbUsedActuators,nbGeneration,nbIndividuals,nbActuationCycle,t,p,nbAvgActuatorsBestIndiv);
+    plotEvolution(performance,sortedPerformance,fitness,...
+        nbAvlbleActuators,nbUsedActuators,nbGeneration,nbIndividuals,...
+        nbActuationCycle,t,p,nbAvgActuatorsBestIndiv);
 end
